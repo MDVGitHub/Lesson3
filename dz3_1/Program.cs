affc1fe991a46fb0e0018986acdf3f59335b1252
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-24 15:59:36 +0300

    Les3

--- dz3_1/Program.cs
+++ dz3_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(Number);
