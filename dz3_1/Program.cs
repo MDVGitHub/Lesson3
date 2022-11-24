d5842962279ffe8fd0c3e2d74bde13df3330f76f
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-24 23:56:49 +0300

    dz3_1

--- dz3_1/Program.cs
+++ dz3_1/Program.cs
@@ -7,5 +7,17 @@
 Console.WriteLine("Введите пятизначное число");
 int Number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
+int n1 = Number / 10000;
+int n2 = Number / 1000 % 10;
+int n3 = Number / 10 % 10;
+int n4 = Number % 10;
 
-Console.WriteLine(Number);
+if (n1 == n4 && n2 == n3)
+{
+    Console.WriteLine($"Число {Number} является палиандром");
+}
+else
+{
+    Console.WriteLine($"Число {Number} не является палиандром");
+}
+Console.WriteLine();
