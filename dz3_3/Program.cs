5c2682f43cdd5936090f43235397d3afdd324139
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-25 00:08:16 +0300

    add

--- dz3_3/Program.cs
+++ dz3_3/Program.cs
@@ -2,3 +2,6 @@
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
+Console.WriteLine("Введите пятизначное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
