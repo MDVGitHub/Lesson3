89ef1b58758eb591285a87e442fe2984bac61eae
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-25 13:21:30 +0300

    dz3_2

--- dz3_2/Program.cs
+++ dz3_2/Program.cs
@@ -2,6 +2,28 @@
 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
-Console.WriteLine("Введите координаты первой точки");
 
-Console.WriteLine("Введите координаты второй точки");
+Console.WriteLine("введите координаты первой точки");
+Console.Write("Введите координаты x1: ");
+int p1x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты y1: ");
+int p1y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты z1: ");
+int p1z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите координаты второй точки");
+Console.Write("Введите координаты x2: ");
+int p2x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты y2: ");
+int p2y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты z2: ");
+int p2z = Convert.ToInt32(Console.ReadLine());
+
+double DistanceToPoint (double p1x, double p1y, double p1z, double p2x, double p2y, double p2z);
+
+    {
+        double distance =  Math.Sqrt((p1x - p2x) * (p1x - p2x) + (p1y - p2y) * (p1y - p2y) + (p1z - p2z) * (p1z - p2z));
+        return distance;
+    }
+
+Console.WriteLine($"Расстояние между точками равно {distance} ");
